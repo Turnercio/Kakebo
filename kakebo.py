337c693fd10248c797c80f42b47ecfb8c32fd508
Rubén Fresneda <cem20814@gmail.com>
2024-04-30 21:50:30 +0200

    test; tipos de datos en fecha y cantidad

--- kakebo.py
+++ kakebo.py
@@ -1,7 +1,15 @@
+from datetime import date
+
 class Ingreso:
     def __init__(self, concepto, fecha, cantidad):
         if not isinstance(concepto, str):
             raise TypeError("Concepto debe ser cadena de texto")
+        if not isinstance(fecha, date):
+            raise TypeError("Fecha tiene que tener el formato correcto: AAAA-MM-DD")
+        if not (isinstance(cantidad, float) or isinstance(cantidad, int)):
+            raise TypeError("Cantidad tiene que ser un numero")
+
+
         self.concepto = concepto
         self.fecha = fecha
         self.cantidad = cantidad
@@ -9,4 +17,3 @@ class Ingreso:
 
 
 
-    
--- test_movimientos.py
+++ test_movimientos.py
@@ -2,7 +2,7 @@ from datetime import date
 from kakebo import Ingreso
 import pytest
 
-def test_instancair_ingresos():
+def test_instanciar_ingresos():
     movimiento = Ingreso("Loteria", date(2024, 1, 5), 1000)
 
     assert movimiento.concepto == "Loteria"
@@ -13,3 +13,12 @@ def test_ingreso_concepto_debe_ser_str():
     with pytest.raises(TypeError):
        movimiento = Ingreso(19, date(2024, 1, 5), 1000)
 
+def test_ingreso_fecha_debe_ser_date():
+    with pytest.raises(TypeError):
+       movimiento = Ingreso("Loteria", "goggooogl", 1000)
+
+def test_ingreso_cantidad_debe_ser_num():
+    with pytest.raises(TypeError):
+        movimiento = Ingreso("Loteria", date.today(), "lola")
+    movimiento = Ingreso("Loteria", date(2024, 1, 5), 1000)
+    movimiento = Ingreso("Loteria", date(2024, 1, 5), 1000,1)
